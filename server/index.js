bc7d029b2983c31fcbdd7c375a1947a8dc1381e3
Jafsari <justinafsari@yahoo.com>
2018-02-20 21:40:51 -0800

    added Models,and ability to save to database while checking to see if user already exists

--- server/index.js
+++ server/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+const keys = require('./config/keys')
+require('./models/User')
 require('./services/passport')
+
 const authRoutes = require('./routes/authroutes')
 
+
+mongoose.connect(keys.mongoURI)
 
 authRoutes(app);
 
