75e599d51805af30a8e9a80c79bd02394c56976b
Jafsari <justinafsari@yahoo.com>
2018-02-22 11:47:12 -0800

    changing structure, adding reducers

---
+++ client/client/src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {} from 'react-router-dom'
+
+const Header = () => <h2> Header </h2>
+const Dashboard = () => <h2> Dashboard </h2>
+const SurveyNew = () => <h2> Survey </h2>
+const Landing = () => <h2> Landing </h2>
+
+
+const App = () => {
+	return (
+			<div> 
+			Hi There!
+			</div>
+		);
+}
+
+export default App;
