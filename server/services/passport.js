146599edd3289b3ebe6ca92c92646b10bbca2ade
Jafsari <justinafsari@yahoo.com>
2018-02-21 20:50:19 -0800

    refactored promises to use async await, added proxy

--- server/services/passport.js
+++ server/services/passport.js
@@ -20,17 +20,14 @@ passport.use(new GoogleStrategy({
   clientID: keys.googleClientID,
   clientSecret: keys.googleClientSecret,
   callbackURL: '/auth/google/callback'
-}, (accessToken,refreshtoken,profile,done) => {
-    User.findOne({ googleId: profile.id })
-    .then((existingUser) => {
+}, 
+async (accessToken,refreshtoken,profile,done) => {
+   const existingUser = await User.findOne({ googleId: profile.id })
       if (existingUser){
       done(null,existingUser)
       } else {
-      new User({ googleId: profile.id })
-      .save()
-      .then(user => done(null,user));
+      const user = await new User({ googleId: profile.id }).save()
+      done(null,user)
       }
-    })
-
-})
-);
+  })
+  );
