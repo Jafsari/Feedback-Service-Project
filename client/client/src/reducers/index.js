9202ebdf6a7bcb0dae6279c75cd6919b77a586bf
Jafsari <justinafsari@yahoo.com>
2018-02-27 16:57:41 -0800

    Rendering list of surveys that was received from api to the dashboard, created surveyList Component

--- client/client/src/reducers/index.js
+++ client/client/src/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from 'redux';
 import auth from './authReducer'
 import { reducer as form} from 'redux-form'
+import surveyReducers from './surveysReducer'
 export default combineReducers({
   auth,
-  form
+  form,
+  surveys:surveyReducers
 });
